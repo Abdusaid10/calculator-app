08c2a9f4ad4360985a8dc0cdf2484d6b25550bc2
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-07-23 13:10:32 +0500

    added styles for Button and ButtonPanel

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // eslint-disable-next-line arrow-body-style
-const Button = ({ name }) => {
+const Button = ({ name, color, wide }) => {
+  const style = {
+    display: 'flex',
+    fontSize: '25px',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: color,
+    width: wide ? '50%' : '25%',
+    borderRight: '3px solid rgb(198, 200, 206)',
+    height: '100%',
+  };
   return (
-    <div>{name}</div>
+    <div style={style}>{name}</div>
   );
 };
 
 Button.propTypes = {
   // eslint-disable-next-line react/no-unused-prop-types
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  color: 'orange',
+  wide: false,
 };
 
 export { Button as default };
