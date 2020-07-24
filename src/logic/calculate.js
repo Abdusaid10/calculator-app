264a252259edf175355d679b61253a15ddd665c4
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-07-24 06:53:29 +0500

    updated calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,7 +2,14 @@ import operate from './operate';
 
 const calculate = ({ total, next, operation }, buttonName) => {
   if (buttonName === '+/-') {
-    [total, next].map(i => i * -1);
+    [total, next].map(i => i * (-1));
+  }
+  if (buttonName === 'AC') {
+    [total, next, operation].map(i => {
+      // eslint-disable-next-line no-param-reassign
+      i = null;
+      return i;
+    });
   }
   return operate(total, next, operation);
 };
