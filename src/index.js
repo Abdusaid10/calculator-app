e5f4971c8277549cd37ca04c1abe052306713d19
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-08-14 22:33:32 +0500

    update README and add LICENSE

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './reset.css';
-import './index.css';
+import './assets/reset.css';
+import './assets/index.css';
 
 ReactDOM.render(
   <App />,
